250ca1716d5bcd24fcc651572ee1064174a60ee8
ahmedrezaakib <arakibid64@gmail.com>
2025-09-26 01:45:07 +0600

    Updated feature

--- app/Http/Controllers/AdminControll.php
+++ app/Http/Controllers/AdminControll.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AdminControll extends Controller
 {
@@ -10,6 +13,38 @@ class AdminControll extends Controller
     {
         return view('admin.login');
     }
+    public function authenticate(Request $req){
+        $req->validate(
+            [
+                'email'=> 'required',
+                'password'=>'required'
+            ]
+            );
+        if(Auth::guard('admin')->attempt(['email'=>$req->email,'password'=>$req->password]))
+        {
+            if(Auth::guard('admin')->user()->role != 'admin'){
+            Auth::guard('admin')->logout();
+            return redirect()->route('admin.login')->with('error','unautherise user');
+
+            }
+            return redirect()->route('admin.login');
+
+        } else{
+            return redirect()->route('admin.login')->with('error','something went wrong');
+        }
+    }
+
+    public function register()
+    {
+        $user = new User();
+        $user->name ='Admin';
+        $user->role ='admin';
+        $user->email ='admin@gmail.com';
+        $user->password = Hash::make('admin');
+        $user->save();
+        return redirect()->route('admin.login')->with('success','user created successfully');
+    }
+
     public function dashboard()
     {
         return view('admin.dashboard');
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('admin/login', [AdminControll::class, 'index'])->name('admin.login');
+Route::get('admin/register', [AdminControll::class, 'register'])->name('admin.register');
+
+Route::post('admin/login', [AdminControll::class, 'authenticate'])->name('admin.authenticate');
+
 Route::get('admin/dashboard', [AdminControll::class, 'dashboard'])->name('admin.dashboard');
 Route::get('admin/form', [AdminControll::class, 'form'])->name('admin.form');
 Route::get('admin/table', [AdminControll::class, 'table'])->name('admin.table');
